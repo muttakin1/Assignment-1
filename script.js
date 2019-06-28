e4f6921061365521e6831df7757753c7933a0003
Muttakin Islam <islammuttakin@live.com>
2019-06-28 19:44:09 +0600

    fully functional navbar

--- script.js
+++ script.js
@@ -3,23 +3,11 @@ submitBtn.addEventListener('click', function(event){
     window.location.assign('home1.html')
 })
 
-var showmap = document.getElementById('show_map')
+$('#aboutus1').click(function () {
+  $('#jumbotron').hide();
 
-showmap.addEventListener('click',function(event){
-    var map = L.map('map').setView([40.91, -96.63], 4);
-
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-
-      var searchControl = L.esri.Geocoding.geosearch().addTo(map);
-
-      var results = L.layerGroup().addTo(map);
-
-      searchControl.on('results', function (data) {
-        results.clearLayers();
-        for (var i = data.results.length - 1; i >= 0; i--) {
-          results.addLayer(L.marker(data.results[i].latlng));
-        }
-      });
-})
+ 
+ 
+ 
+ 
+  })
